59da33d36f56d5bd59fee709e935390ee704c341
Wahaj Javed <84095994+WahajJaved20@users.noreply.github.com>
2024-04-08 23:23:16 +0500

    payment field onnn

--- src/Components/SubmissionForm.jsx
+++ src/Components/SubmissionForm.jsx
@@ -1,6 +1,73 @@
-import React from "react";
+import React, {useState} from "react";
 import HOC from "./HOC";
+import { useDropzone } from 'react-dropzone'
+import { RxCross2 } from "react-icons/rx";
 import { abstractIcon, uniquenessIcon, featuresIcon } from "../assets";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "./CardUI";
+function PaymentInfo(){
+
+  const info = {
+      title: "Muhammad Shahmir Raza",
+      number: "03188409943",
+      amount:1000, 
+      // iban: "PK94FAYS0169007900161339",
+      // bcode: "169",
+      bank_name: "Nayapay",
+  }
+
+  return (
+      <div className=' flex flex-col gap-1 md:gap-2 my-2 md:my-3 rounded-xl text-white p-3 md:p-5 bg-gray-800'>
+          <div className='text-lg md:text-xl mb-5'>Pay Fees in the following Account and upload the receipt</div>
+          <div className='flex flex-row gap-5'>
+              <div className='text-gray-400'>Bank Name: </div>
+              <div className=''>{info.bank_name}</div>
+          </div>
+          <div className='flex flex-row gap-5'>
+              <div className='text-gray-400'>Account Title: </div>
+              <div className=''>{info.title}</div>
+          </div>
+          <div className='flex flex-row gap-5'>
+              <div className='text-gray-400'>Account Number: </div>
+              <div className=''>{info.number}</div>
+          </div>
+          <div className='flex flex-row gap-5'>
+              <div className='text-gray-400'>Amount: </div>
+              <div className=''>{info.amount}</div>
+          </div>
+{/*             <div className='flex flex-row gap-5'>
+              <div className='text-gray-400'>IBAN: </div>
+              <div className=''>{info.iban}</div>
+          </div>
+          <div className='flex flex-row gap-5'>
+              <div className='text-gray-400'>Branch Code: </div>
+              <div className=''>{info.bcode}</div>
+          </div> */}
+      </div>
+  )
+}
+
+function UploadCloudIcon(props) {
+  return (
+      <svg
+          {...props}
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+      >
+          <path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242" />
+          <path d="M12 12v9" />
+          <path d="m16 16-4-4-4 4" />
+      </svg>
+  )
+}
+
+
   const formContent = [
     {
         header: 'TEAM LEADER',
@@ -25,10 +92,20 @@ import { abstractIcon, uniquenessIcon, featuresIcon } from "../assets";
         uniqueness: '',
         features: '',
         teamName: '',
+        university: '',
     }
 ]
   function TeamSection({reference}) {
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+      accept: 'image/png, image/jpeg',
+      maxFiles: 1,
+      onDrop: (acceptedFiles) => {
+          setFiles(acceptedFiles[0])
+      },
+  })
+
     const [formData, setFormData] = React.useState(formContent);
+    const [files, setFiles] = useState(null)
     const teamMembers = [
       {
         header: 'TEAM LEADER',
@@ -65,10 +142,16 @@ import { abstractIcon, uniquenessIcon, featuresIcon } from "../assets";
       submissionFormData['abstract'] = formData[3].abstract;
       submissionFormData['uniqueness'] = formData[3].uniqueness;
       submissionFormData['features'] = formData[3].features;
+      submissionFormData["university_name"] = formData[3].university;
       submissionFormData["fees_amount"] = 1000;
       submissionFormData["category"] = "FYP";
       submissionFormData["paid"] = false;
       submissionFormData["reference_code"] = "null";
+      submissionFormData["payment_receipt"] = files;
+      if (files === null) {
+        alert('Please upload a payment receipt')
+        return
+    }
       try{
         await fetch('https://api.acmdevday.com/addFYPRegistration', {
           method: 'POST',
@@ -97,6 +180,28 @@ import { abstractIcon, uniquenessIcon, featuresIcon } from "../assets";
               onChange={(e) => {
                   const newFormData = [...formData];
                   newFormData[3].teamName = e.target.value;
+                  setFormData(newFormData);
+              }
+              }
+              type="text"
+              style={{
+                  padding: '0px',
+                  border: 'none',
+                  borderBottom: '2px solid black',
+                  backgroundColor:"transparent",
+                  outline: 'none',
+                  fontSize: '24px',
+              }}
+              className="font-Gotham"
+              />
+      </div>
+      <div className="mt-8 justify-center flex flex-col px-8">
+        <h1 className="font-Gotham text-2xl text-black font-bold mb-2 mr-8">UNIVERSITY NAME: </h1>
+        <input
+              value={formData[3].university}
+              onChange={(e) => {
+                  const newFormData = [...formData];
+                  newFormData[3].university = e.target.value;
                   setFormData(newFormData);
               }
               }
@@ -235,8 +340,74 @@ import { abstractIcon, uniquenessIcon, featuresIcon } from "../assets";
               </div>
          
         </div>
+        <Card className="bg-white border-4 border-black md:w-4/6 m-5 justify-center ml-[20%]">
+                    <CardHeader>
+                        <CardTitle className="text-headerColorTwo mx-auto mb-3">
+                            UPLOAD PAYMENT RECEIPT
+                        </CardTitle>
+                        <CardDescription>
+                            <PaymentInfo/>
+                            Drag and drop your image or click the button below
+                            to select file.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent
+                        {...getRootProps()}
+                        className={`flex flex-col items-center justify-center border-2 m-1  ${
+                            isDragActive
+                                ? 'border-blue-500'
+                                : 'border-zinc-500 dark:border-zinc-800'
+                        } border-dashed rounded-lg space-y-3`}
+                    >
+                        <input {...getInputProps()} />
+
+                        {isDragActive ? (
+                            <p className="text-blue-500">
+                                Drop the file here...
+                            </p>
+                        ) : (
+                            <>
+                                <UploadCloudIcon className="w-16 h-16 text-zinc-500 dark:text-zinc-400 " />
+                                <p className="text-gray-500 ">
+                                    Drag & drop image here, or click to
+                                    select image
+                                </p>
+                            </>
+                        )}
+
+                        {files && files.type && files.name && (
+                            <div className="relative h-44 md:h-72 overflow-hidden round-md">
+                                <img
+                                    src={
+                                        files.type.startsWith('image/')
+                                            ? URL.createObjectURL(files)
+                                            : ''
+                                    }
+                                    alt={files.name}
+                                    className="rounded-lg"
+                                />
+                               <div
+                                    className="border absolute top-2 right-2 m-1 rounded-full p-2 z-1000"
+                                    onMouseEnter={(e) => (e.currentTarget.style.backgroundColor = '#2e4f7cdb')}
+                                    onMouseLeave={(e) => (e.currentTarget.style.backgroundColor = 'transparent')}
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        e.preventDefault()
+                                        setFiles(null)
+                                    }}
+                                >
+                                    <RxCross2 className="text-black text-xl" />
+                                </div>
+                            </div>
+                        )}
+                    </CardContent>
+                </Card>
         <div>
-        <button onClick={handleSubmit} disabled={formData.some((data) => Object.values(data).some((value) => value === ''))}
+        <button onClick={()=>{
+            handleSubmit();
+            alert("Registration Successful")
+            window.location.reload();
+        }} 
         style={{
             boxShadow: '8px 8px 0px rgba(0, 0, 0, 1)',
         }}
